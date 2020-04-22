0c871305248d61ce8aa1786c34a03402e7feaf0d
Roy Stegeman <roystegeman@live.nl>
2020-04-23 00:52:21 +0200

    add CMA test, and speed up test_fit

--- src/evolutionary_keras/tests/test_fit.py
+++ src/evolutionary_keras/tests/test_fit.py
@@ -4,7 +4,7 @@ import numpy as np
 from tensorflow.keras.layers import Dense, Input
 
 from evolutionary_keras.models import EvolModel
-from evolutionary_keras.optimizers import NGA
+from evolutionary_keras.optimizers import NGA, CMA
 
 
 def generate_model(ishape=2, hshape=4, oshape=1):
@@ -18,8 +18,8 @@ def generate_model(ishape=2, hshape=4, oshape=1):
 
 
 def test_NGA():
-    """ Test the NGA is able to run fit """
-    optimizer = NGA(mutation_rate=1.0)
+    """ Tests whether the NGA is able to run fit """
+    optimizer = NGA(mutation_rate=1.0, population_size=10)
     modelito = generate_model(ishape=2, hshape=4, oshape=1)
     modelito.compile(optimizer=optimizer, loss="mse")
     # Since we just want to check the optimizer is able to run
@@ -27,6 +27,20 @@ def test_NGA():
     xin = np.ones((100, 2))
     yout = np.zeros((100, 1))
     start_loss = modelito.evaluate(x=xin, y=yout)
-    _ = modelito.fit(x=xin, y=yout, epochs=50)
+    _ = modelito.fit(x=xin, y=yout, epochs=10)
+    final_loss = modelito.evaluate(x=xin, y=yout)
+    assert final_loss < start_loss
+
+def test_CMA():
+    """ Tests whether the CMA is able to run fit """
+    optimizer = CMA(max_evaluations=10)
+    modelito = generate_model(ishape=2, hshape=4, oshape=1)
+    modelito.compile(optimizer=optimizer, loss="mse")
+    # Since we just want to check the optimizer is able to run
+    # Give some arbitrary input and output for 50 epochs
+    xin = np.ones((100, 2))
+    yout = np.zeros((100, 1))
+    start_loss = modelito.evaluate(x=xin, y=yout)
+    _ = modelito.fit(x=xin, y=yout)
     final_loss = modelito.evaluate(x=xin, y=yout)
     assert final_loss < start_loss
